823a4c4b40e45cae5f338f7026c9fdf87d0094ef
1060269130@qq.com <1060269130@qq.com>
2021-06-09 22:17:04 +0800

    js切换成ts

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,5 +1,20 @@
 import React from "react";
-export const SearchPanel = ({ users, param, setParam }) => {
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
     <form>
       <div>
@@ -14,7 +29,9 @@ export const SearchPanel = ({ users, param, setParam }) => {
         >
           <option value={""}>负责人</option>
           {users.map((user) => (
-            <option key={user.id} value={user.id}>{user.name}</option>
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
           ))}
         </select>
       </div>
